0b955915cc7a5bd563cf87a1a1e2fd81ff16dad1
Sebastian Prein <hi@sebastianprein.com>
2016-01-17 23:35:03 +0100

    adjusted wording in build task

--- tasks/build.js
+++ tasks/build.js
@@ -1,12 +1,13 @@
 var config  = require('../project.json');
-var colors  = require('colors');
-var async   = require('async');
 var styles  = require('../tasks/build-styles');
 var scripts = require('../tasks/build-scripts');
 var html    = require('../tasks/build-html');
 var images  = require('../tasks/build-images');
 var sprites = require('../tasks/build-sprites');
 var clean   = require('../tasks/clean');
+
+var colors  = require('colors');
+var async   = require('async');
 
 // measure total build time
 var start = Date.now();
@@ -15,8 +16,8 @@ var start = Date.now();
 var mode = (config.env === 'prod' ? 'production' : 'develop').bold.blue;
 
 // tasks to run in chronological order
-var tasks = [ clean, styles, scripts, html, images, sprites ].map(function(task) {
-    return function(cb) {
+var tasks = [ clean, styles, scripts, html, images, sprites ].map((task) => {
+    return (cb) => {
         task.run({
             done: cb
         });
@@ -24,7 +25,7 @@ var tasks = [ clean, styles, scripts, html, images, sprites ].map(function(task)
 });
 
 // function to run once building finished
-var done = function() {
+var done = () => {
     var hr = String.fromCharCode(8212).repeat(process.stdout.columns * 0.125).bold.green;
     var duration = Date.now() - start;
 
@@ -33,7 +34,7 @@ var done = function() {
 };
 
 // log environment
-console.log(`Running in ${mode} mode...\n`);
+console.log(`Building in ${mode} mode...\n`);
 
 // run tasks
 async.series(tasks, done);
